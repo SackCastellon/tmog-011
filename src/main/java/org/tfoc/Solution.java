2bb260186533f34ccf378b508b646122c148d400
Juan José González Abril <juanjo@gonzabril.es>
2024-04-07 18:49:22 +0200

    Implement solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,16 +1,34 @@
 package org.tfoc;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public List<String> letterCombinations(String digits) {
+    private static final Map<Integer, List<String>> letters = Map.of(
+        2, List.of("a", "b", "c"),
+        3, List.of("d", "e", "f"),
+        4, List.of("g", "h", "i"),
+        5, List.of("j", "k", "l"),
+        6, List.of("m", "n", "o"),
+        7, List.of("p", "q", "r", "s"),
+        8, List.of("t", "u", "v"),
+        9, List.of("w", "x", "y", "z")
+    );
 
-        return Collections.emptyList();
+    public List<String> letterCombinations(String digits) {
+        return digits.chars()
+            .map(Character::getNumericValue)
+            .mapToObj(letters::get)
+            .reduce(List.of(), this::combine);
+    }
+
+    private List<String> combine(List<String> heads, List<String> tails) {
+        if (heads.isEmpty()) return tails;
+        return heads.stream().flatMap(head -> tails.stream().map(head::concat)).toList();
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,42 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
 
     @Test
-    void testSolution() {
+    void example1() {
+        var result = solution.letterCombinations("23");
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        assertThat(result).containsExactly("ad","ae","af","bd","be","bf","cd","ce","cf");
+    }
+
+    @Test
+    void example2() {
+        var result = solution.letterCombinations("");
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void example3() {
+        var result = solution.letterCombinations("2");
+
+        assertThat(result).containsExactly("a","b","c");
     }
 
 }
